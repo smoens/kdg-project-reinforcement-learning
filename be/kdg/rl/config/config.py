c0c679cfdec5bb856deb372781eb10d5388e4df8
smoens <sarah_m@telenet.be>
2021-06-11 08:27:58 +0200

    added placeholders for model

---
+++ be/kdg/rl/config/config.py
@@ -0,0 +1,11 @@
+############# PARAMETERS #############
+params = {
+    "model1": {
+        "model_filename": "model1.h5",
+        "lr": 0.001,
+        "num_epochs": 10,
+        "validation_steps": 10,
+        "metrics": ["acc"],
+        "seed": 1354874613,
+    }
+}
---
+++ be/kdg/rl/model/model1.py
@@ -0,0 +1,25 @@
+from keras import layers
+from keras import models
+from keras import optimizers
+
+from be.kdg.rl.config import config
+
+
+# Cartpole DQN https://www.youtube.com/watch?v=D795oNqa-Vk
+
+def createModel(selectedModel):
+    ############# MODEL #############
+    model = models.Sequential()
+    model.add(layers.Dense(256, activation='relu', input_shape=[]))
+        # TODO add correct input spaces...
+    model.add(layers.Dense(64, activation='relu', input_shape=[]))
+        # TODO add layer size of action_size
+    #model.add(layers.Dense(action_size, activation='softmax'))
+
+    model.summary()
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizers.RMSprop(lr=1e-4),
+                  metrics=config.params.get(selectedModel).get("metrics"))
+
+    return model
