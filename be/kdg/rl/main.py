b23b2f83c6086400ebe03d135d2751b7fc132e4e
smoens <sarah_m@telenet.be>
2021-06-12 18:26:50 +0200

    minor changes

--- be/kdg/rl/main.py
+++ be/kdg/rl/main.py
@@ -3,8 +3,10 @@ from be.kdg.rl.environment.openai import FrozenLakeEnvironment, FrozenLakeNotSli
 from be.kdg.rl.learning.tabular.qlearning import Qlearning, NStepQlearning, MonteCarloLearning
 from be.kdg.rl.learning.approximate.deep_qlearning import DeepQLearning
 from be.kdg.rl.agent.episode import Episode
+import matplotlib
 
 if __name__ == '__main__':
+    matplotlib.use("TkAgg")                 #activate tkinter
     environment = FrozenLakeEnvironment()
     agent: Agent = TabularAgent(environment, Qlearning(environment))
     agent.train()
