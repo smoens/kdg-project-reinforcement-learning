7aebe455e3b0fb977a71787a727132f32d8e426b
smoens <sarah_m@telenet.be>
2021-06-13 11:23:30 +0200

    parametrize experiments

--- be/kdg/rl/main.py
+++ be/kdg/rl/main.py
@@ -3,10 +3,16 @@ from be.kdg.rl.environment.openai import FrozenLakeEnvironment, FrozenLakeNotSli
 from be.kdg.rl.learning.tabular.qlearning import Qlearning, NStepQlearning, MonteCarloLearning
 from be.kdg.rl.learning.approximate.deep_qlearning import DeepQLearning
 from be.kdg.rl.agent.episode import Episode
+from be.kdg.rl.utils.config import params
 import matplotlib
 
 if __name__ == '__main__':
     matplotlib.use("TkAgg")                 #activate tkinter
-    environment = FrozenLakeEnvironment()
-    agent: Agent = TabularAgent(environment, Qlearning(environment))
+    experiment = "experiment2"
+
+    learning = params.get("experiment").get(experiment).get("learning")
+    env_name = params.get("experiment").get(experiment).get("environment") + "()"
+    nstep = params.get("experiment").get(experiment).get("nstep")
+    environment = eval(env_name)
+    agent: Agent = eval("TabularAgent(environment," + learning + "(environment" + ("))" if nstep is None else nstep + "))"))
     agent.train()
--- be/kdg/rl/utils/config.py
+++ be/kdg/rl/utils/config.py
@@ -1,7 +1,36 @@
 ############# PARAMETERS #############
 params = {
-    "experiments": {
-
+    "experiment": {
+        "default": {
+            'description': 'Experiment with default settings',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.7,
+            'λ': 0.0005,
+            'γ': 0.9,
+            't_max': 99
+        },
+        "experiment1": {
+            'description': 'Experiment with completely random agent',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.7,
+            'λ': 0.0005,
+            'γ': 0.9,
+            't_max': 99
+        },
+        "experiment2": {
+            'description': 'Experiment with lower discount rate',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.7,
+            'λ': 0.0005,
+            'γ': 0.5,
+            't_max': 99
+        }
     },
     "models": {
         "model1": {
@@ -12,5 +41,16 @@ params = {
             "metrics": ["acc"],
             "seed": 1354874613,
         }
-    }
+    },
+    "environments": {
+        "FrozenLake": "FrozenLakeEnvironment",
+        "CartPole": "CartPoleEnvironment"
+    },
+    "methods": [
+        "Qlearning",
+        "NStepQLearning",
+        "MonteCarloLearning",
+        "DeepQLearning",
+        "DoubleDeepQLearning"
+    ]
 }
