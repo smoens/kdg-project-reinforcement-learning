73b1e218c05c09dc49bef442ff19807aecd09c83
smoens <sarah_m@telenet.be>
2021-06-13 14:59:44 +0200

    remove n from montecarlo (derived value from episode)

--- be/kdg/rl/learning/tabular/qlearning.py
+++ be/kdg/rl/learning/tabular/qlearning.py
@@ -79,7 +79,7 @@ class NStepQlearning(TabularLearner):
 
 class MonteCarloLearning(TabularLearner):
 
-    def __init__(self, environment: Environment, n: int, α=0.7, λ=0.0005, γ=0.9, t_max=99) -> None:
+    def __init__(self, environment: Environment, α=0.7, λ=0.0005, γ=0.9, t_max=99) -> None:
         TabularLearner.__init__(self, environment, α, λ, γ, t_max)
         self.percepts = []  # this will buffer the percepts
 
