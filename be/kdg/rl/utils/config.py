c615d83137c4313ac2d66498cecb421ac41fe6c6
smoens <sarah_m@telenet.be>
2021-06-13 13:39:52 +0200

    add experiments

--- be/kdg/rl/utils/config.py
+++ be/kdg/rl/utils/config.py
@@ -1,12 +1,12 @@
 import os
 
 ############# PARAMETERS #############
-current_experiment = "experiment3"
+current_experiment = "experiment6"
 n_episodes = 10000
 output_freq = 100      # define at what frequency of episodes we want to create output
 
-def init():
-    init_folders(current_experiment)
+def init(environment):
+    init_folders(environment, experiment=current_experiment)
 
 
 params = {
@@ -38,6 +38,16 @@ params = {
             't_max': 99
         },
         "experiment2": {
+            'description': 'Experiment QLearning',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.7,
+            'λ': 0.0005,
+            'γ': 0.9,
+            't_max': 99
+        },
+        "experiment3": {
             'description': 'Experiment QLearning with lower discount rate',
             'environment': 'FrozenLakeEnvironment',
             'learning': 'Qlearning',
@@ -47,21 +57,71 @@ params = {
             'γ': 0.5,
             't_max': 99
         },
-        "experiment3": {
+        "experiment4": {
+            'description': 'Experiment QLearning with decreased learning rate',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.5,
+            'λ': 0.0005,
+            'γ': 0.5,
+            't_max': 99
+        },
+        "experiment5": {
+            'description': 'Experiment QLearning with very small learning rate',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.2,
+            'λ': 0.0005,
+            'γ': 0.5,
+            't_max': 99
+        },
+        "experiment6": {
+            'description': 'Experiment QLearning with large learning rate',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.9,
+            'λ': 0.0005,
+            'γ': 0.5,
+            't_max': 99
+        },
+        "experiment7": {
+            'description': 'Experiment QLearning with very small discount rate',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'Qlearning',
+            'nstep': None,
+            'α': 0.7,
+            'λ': 0.0005,
+            'γ': 0.1,
+            't_max': 99
+        },
+        "experiment8": {
             'description': 'Experiment NStepQLearning',
             'environment': 'FrozenLakeEnvironment',
             'learning': 'NStepQlearning',
-            'nstep': '5',
+            'nstep': '3',
+            'α': 0.7,
+            'λ': 0.0005,
+            'γ': 0.9,
+            't_max': 99
+        },
+        "experiment6": {
+            'description': 'Experiment NStepQLearning with lower discount',
+            'environment': 'FrozenLakeEnvironment',
+            'learning': 'NStepQlearning',
+            'nstep': '3',
             'α': 0.7,
             'λ': 0.0005,
             'γ': 0.5,
             't_max': 99
         },
-        "experiment4": {
+        "experiment7": {
             'description': 'Experiment NStepQLearning with larger step-size',
             'environment': 'FrozenLakeEnvironment',
             'learning': 'NStepQlearning',
-            'nstep': 10,
+            'nstep': '7',
             'α': 0.7,
             'λ': 0.0005,
             'γ': 0.5,
@@ -92,8 +152,11 @@ params = {
 }
 
 
-def init_folders(experiment):
-    path_experiment = os.path.join(params.get("dirs").get("output"), experiment)
+def init_folders(env, experiment):
+    path_experiment = os.path.join(
+        params.get("dirs").get("output"),
+        params.get("experiment").get(current_experiment).get("environment"),
+        experiment)
     path_qval = params.get("dirs").get("qval")
     path_reward = params.get("dirs").get("reward")
     if not os.path.exists(path_experiment):
